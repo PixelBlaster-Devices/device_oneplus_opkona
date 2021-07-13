3e8cfeccc528df2b988a42ee9caf6d9e75e2bfc3
Chris <cjh1249131356@gmail.com>
2021-07-13 19:49:30 +0800

    sm8250-common: DeviceSettings: Allow mute media volume on silent mode
    * Allow users set to automatically mute media volume when alert slider
      switched to silent mode. (Only when using speaker for audio output)
    
    Signed-off-by: Chris <cjh1249131356@gmail.com>

--- DeviceSettings/src/org/havoc/device/DeviceSettings/KeyHandler.java
+++ DeviceSettings/src/org/havoc/device/DeviceSettings/KeyHandler.java
@@ -49,28 +49,18 @@ public class KeyHandler implements DeviceKeyHandler {
 
         switch (scanCode) {
             case MODE_NORMAL:
-                if (currentRingerMode != AudioManager.RINGER_MODE_NORMAL) {
-                    doHapticFeedback(VibrationEffect.EFFECT_DOUBLE_CLICK);
-                    mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
-                } else {
-                    return event;
-                }
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
+                doHapticFeedback(MODE_NORMAL_EFFECT);
+                VolumeService.changeMediaVolume(mAudioManager, mContext);
                 break;
             case MODE_VIBRATION:
-                if (currentRingerMode != AudioManager.RINGER_MODE_VIBRATE) {
-                    doHapticFeedback(VibrationEffect.EFFECT_THUD);
-                    mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
-                } else {
-                    return event;
-                }
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
+                doHapticFeedback(MODE_VIBRATION_EFFECT);
+                VolumeService.changeMediaVolume(mAudioManager, mContext);
                 break;
             case MODE_SILENCE:
-                if (currentRingerMode != AudioManager.RINGER_MODE_SILENT) {
-                    doHapticFeedback(VibrationEffect.EFFECT_POP);
-                    mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
-                } else {
-                    return event;
-                }
+                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
+                VolumeService.changeMediaVolume(mAudioManager, mContext);
                 break;
             default:
                 return event;
