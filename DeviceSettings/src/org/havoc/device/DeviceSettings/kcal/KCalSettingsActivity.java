c38ccf1bbee1bf40e00635874a84c55a3727dfa6
SKULSHADY <anushekprasal@gmail.com>
2021-09-13 15:15:52 +0530

    sm8250-common: DeviceSettings: Add a scrollable image view layout * For Kcal settings * preview images taken from Franco Kernel Manager
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- DeviceSettings/src/org/havoc/device/DeviceSettings/kcal/KCalSettingsActivity.java
+++ DeviceSettings/src/org/havoc/device/DeviceSettings/kcal/KCalSettingsActivity.java
@@ -19,20 +19,32 @@ package org.havoc.device.DeviceSettings.kcal;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
+import org.havoc.device.DeviceSettings.ViewPagerAdapter;
 import org.havoc.device.DeviceSettings.R;
 
 public class KCalSettingsActivity extends Activity implements Utils {
 
+    private ViewPager viewPager;
+    private LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
     private KCalSettings mKCalSettingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kcal);
+
+        setupImageSlider();
 
         Fragment fragment = getFragmentManager().findFragmentById(R.id.fragment_kcal);
         if (fragment == null) {
@@ -43,6 +55,48 @@ public class KCalSettingsActivity extends Activity implements Utils {
         } else {
             mKCalSettingsFragment = (KCalSettings) fragment;
         }
+    }
+
+    private void setupImageSlider() {
+        viewPager = (ViewPager) findViewById(R.id.preview);
+        sliderDotspanel = (LinearLayout) findViewById(R.id.SliderDots);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getApplicationContext());
+        viewPager.setAdapter(viewPagerAdapter);
+
+        dotscount = viewPagerAdapter.getCount();
+        dots = new ImageView[dotscount];
+
+        for(int i = 0; i < dotscount; i++) {
+            dots[i] = new ImageView(getApplicationContext());
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.inactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 0, 8, 0);
+            sliderDotspanel.addView(dots[i], params);
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i< dotscount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.inactive_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
     }
 
     @Override
