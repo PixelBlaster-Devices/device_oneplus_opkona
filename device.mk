cb67a7ac5e9c992eaadfa2cd49a743caad5d13b1
aswin7469 <aswinas@pixysos.com>
2021-10-28 15:43:15 +0200

    sm8250-common: enable Smooth Display feature in Settings for aosp
    Signed-off-by: Rohan Hasabe <rohanhasabe8@gmail.com>

--- device.mk
+++ device.mk
@@ -661,6 +661,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.refresh_rate_switching=true \
     ro.surface_flinger.use_color_management=true \
     ro.surface_flinger.wcg_composition_dataspace=143261696
