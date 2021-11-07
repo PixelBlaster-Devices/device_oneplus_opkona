e1ee491021fa43b66d8f862a28035051cd9f9eb8
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-11-07 09:11:48 +0530

    Revert "lemonkebab: Switch to AOSP WFD"
    This reverts commit 140bb77c026081c700b99153b06a0905bbc5728c.

--- device.mk
+++ device.mk
@@ -631,7 +631,14 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl
+    libnl \
+    libwfdaac_vendor
+
+#PRODUCT_BOOT_JARS += \
+ #   WfdCommon
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
 
 # Wifi Overlay
 PRODUCT_PACKAGES += \
