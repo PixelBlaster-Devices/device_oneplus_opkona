6cb88ccd3b130bb918b587f623465535738b23d3
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-01-20 18:31:00 +0530

    lemonkebab: libperfmgr: Nuke uclamp

--- device.mk
+++ device.mk
@@ -507,7 +507,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
-    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # QMI
