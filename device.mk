fef0da80d1dee01f600b6557b52ed65c80811298
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-02-07 11:56:46 +0530

    lemonkebab: Build wifi packages

--- device.mk
+++ device.mk
@@ -203,6 +203,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwpa_client \
+    libwifi-hal-ctrl \
+    libwifi-hal-qcom \
+    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
 # API Level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
