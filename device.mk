c13c790d001318a6e8196cb4b5f92bbb0a5c32b9
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-02-07 11:23:29 +0530

    lemonkebab: Build Bluetooth packages

--- device.mk
+++ device.mk
@@ -61,6 +61,16 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    audio.bluetooth.default \
+    android.hardware.bluetooth.audio@2.1-impl \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbluetooth_audio_session \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
