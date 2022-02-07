b507b0b0b05be890203148fcc1d7be74c7486099
Christian Oder <myself5@carbonrom.org>
2022-02-07 14:24:53 +0530

    lemonkebab: Patch fingerprint blobs to a different property.  * libgf_ud_hal.so looks for verified boot state to be set to the phones true state in our case, for an unlocked phone, it must be orange green means it is locked with the factory ROM, orange means unlocked no checking, yellow = locked but with a user key.
     * Change it to look for vendor.boot.fingerprintbstate which we set to orange without having to mess with anything SafetyNet related.
    
    Co-authored-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: Ifcdaf57a8100219529d97bef2c1195f01ce2e105

--- extract-files.sh
+++ extract-files.sh
@@ -54,6 +54,11 @@ if [ -z "${SRC}" ]; then
 fi
 
 function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libgf_ud_hal.so|vendor/lib/libgf_g6_ud_hal.so|vendor/lib64/libgf_ud_hal.so|vendor/lib64/libgf_g6_ud_hal.so)
+            sed -i "s|vendor.boot.verifiedbootstate|vendor.boot.fingerprintbstate|g" "${2}"
+            ;;
+    esac
 }
 
 # Initialize the helper for common device
