eb0cc930c11cfbf59382b9f170afc48bf6801604
YumeMichi <do4suki@gmail.com>
2022-02-07 13:33:00 +0530

    lemonkebab: Update blobs from LA.UM.9.12.r1-10800-SMxx50.0
    - BT stack is from LA.UM.9.14.r1-18400.02-LAHAINA.QSSI12.0
    
    lemonkebab: Update keymaster blobs from LA.UM.9.12.r1-10800-SMxx50.0

--- device.mk
+++ device.mk
@@ -176,6 +176,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# Keymaster
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.strongbox_keystore.xml
+
 # Overlays
 PRODUCT_PACKAGES += \
     OverlayFrameworks \
