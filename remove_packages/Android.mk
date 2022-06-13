769d36510d3ce955333591491c87ba98620b1719
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-06-13 12:56:26 +0530

    lemonkebab: Override FM2
    - IDK why it is getting built

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Camera2 GalleryGoPrebuilt Gallery2 Photos Snap
+LOCAL_OVERRIDES_PACKAGES := Camera2 GalleryGoPrebuilt Gallery2 Photos Snap FM2
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
