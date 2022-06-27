758ba5a37ab59c96a004d0efd776565f5eb3f164
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-06-27 11:48:29 +0530

    lemonkebab: Changes for OOS12
    - Change config
    - Permissive for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,8 +78,8 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
+BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
@@ -90,7 +90,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM=1
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-KERNEL_DEFCONFIG := vendor/kona-perf_defconfig
+KERNEL_DEFCONFIG := vendor/Illusion_defconfig
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
