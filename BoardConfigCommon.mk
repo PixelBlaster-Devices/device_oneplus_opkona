a13b69ed9323e628a77f656829050a7d51b14cb4
Yuan Si <do4suki@gmail.com>
2022-09-04 03:26:57 +0800

    sm8250-common: Configure test<->release key migration
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: Ic6cccb4cfdfe24865c4f720f8edf4a507548b78c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,6 +51,7 @@ TARGET_BOOTLOADER_BOARD_NAME := kona
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Fingerprint
