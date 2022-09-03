d51d25e29e635cbf71b584c627ef981249b50574
Yuan Si <do4suki@gmail.com>
2022-09-03 15:28:09 +0800

    sm8250-common: Switch to QTI Telephony component
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I1764cd62e486d498de59db3d69c9f008d7ad2fa4

--- common.mk
+++ common.mk
@@ -200,6 +200,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     media-legacy \
     nq-nfc \
     perf \
+    telephony \
     usb \
     wfd
 
@@ -243,13 +244,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oplus
-
-# Telephony
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # Update engine
 PRODUCT_PACKAGES += \
