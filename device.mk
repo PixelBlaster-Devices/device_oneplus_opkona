c1c86b9d51c40e9cdf38474f072ad1a352618787
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-10-23 19:59:00 +0530

    lemonkebab: Unify with 8/8P
    - Add 8/8P specific blobs
    - Add init script to overlay the device specific blobs based on props
    
    Co-authored-by: IPSBHANGU <inderpreetbhangu2@gmail.com>

--- device.mk
+++ device.mk
@@ -165,7 +165,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.target.rc \
     ueventd.oplus.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init_overlayfs.rc
 
 # Keymaster
 PRODUCT_PACKAGES += \
