160ea4fe81347284157c6485aabb226defd70cf0
Yuan Si <do4suki@gmail.com>
2022-11-13 13:55:29 +0530

    opkona: Update blobs from LA.UM.9.12.r1-14400-SMxx50.QSSI12.0
    - Also switch telephony to qti common component
    - Also update hashes of pinned blobs
    
    Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>

--- device.mk
+++ device.mk
@@ -241,6 +241,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     media-legacy \
     nq-nfc \
     perf \
+    telephony \
     usb \
     wfd
 
@@ -284,13 +285,6 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/oplus
-
-# Telephony
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # Touch
 PRODUCT_PACKAGES += \
